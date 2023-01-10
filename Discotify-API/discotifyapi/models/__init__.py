b23c5837611b7a98d5b66b0b5868bd7357d92b64
Justin Ferwerda <justin.ferwerda@gmail.com>
2023-01-10 10:31:23 -0600

    made migrations and migrated models into DB

--- Discotify-API/discotifyapi/models/__init__.py
+++ Discotify-API/discotifyapi/models/__init__.py
@@ -1,3 +1,8 @@
 """import to init for availability"""
 
 from .user import User
+from .album import Album
+from .genre import Genre
+from .trade import Trade
+from .track import Track
+from .wishlist import Wishlist
